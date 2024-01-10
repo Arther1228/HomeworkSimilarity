b0027ae9e8580c9af53151fe252a71140d0a3098
arther606 <396986654@qq.com>
2024-01-10 08:55:05 +0800

    修改备注

--- src/main/java/pers/hdq/ui/UiUtil.java
+++ src/main/java/pers/hdq/ui/UiUtil.java
@@ -9,14 +9,16 @@ import java.awt.*;
 public class UiUtil {
 
     /**
-     *  初始化标签页
+     * 初始化标签页
      */
-    public static JTabbedPane initJTabbedPane(){
+    public static JTabbedPane initJTabbedPane() {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setBounds(10, 55, 759, 300);
 
         JPanel excelComparisonPanel = CampareItemUI.createExcelComparisonPanel();
         tabbedPane.addTab("Excel文件比较", null, excelComparisonPanel, "设置比较项");
+        JComponent wordTxt = makeTextPanel("直接选择比较的文件，点击开始比较，即可按照默认的两两文件进行比较相似度");
+        tabbedPane.addTab("Word/Txt文件比较", null, wordTxt, "");
         JTextPane jDescriptionPane = UiUtil.initJTextPane();
         tabbedPane.addTab("比较说明", null, jDescriptionPane, "说明内容");
 
@@ -25,10 +27,21 @@ public class UiUtil {
         return tabbedPane;
     }
 
+    public static JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        filler.setFont(new Font("仿宋", Font.PLAIN, 16));
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+
+        return panel;
+    }
+
     /**
-     *  初始化使用说明
+     * 初始化使用说明
      */
-    public static JTextPane initJTextPane(){
+    public static JTextPane initJTextPane() {
 
         JTextPane txtpnrnrncsvexcelrn = new JTextPane();
         txtpnrnrncsvexcelrn.setBackground(SystemColor.controlHighlight);
@@ -37,9 +50,9 @@ public class UiUtil {
         txtpnrnrncsvexcelrn.setEditable(false);
         txtpnrnrncsvexcelrn.setText("使用说明：\r\n  1.相似度比对前请将待相似度比对文档放入文件夹中，然后点击“选择比对路径”按钮选择该文件夹，点击“开始相似度比对”按钮，开始比对；" +
                 "\n  2.相似度比对结果存储于所选文件夹中以“相似度比对结果”开头的Excel表格中；" +
-                "\n  3.“简略结果”表列出每个文件及其最相似文件，详细结果表列出全部结果；超过相似度阈值名单会列出相似度超过在选定阈值的文件名。"+
+                "\n  3.“简略结果”表列出每个文件及其最相似文件，详细结果表列出全部结果；超过相似度阈值名单会列出相似度超过在选定阈值的文件名。" +
                 "\n  4.Word/txt文件：目前只支持整个文件两两比较，不支持选择文本段落。" +
-                "\n  4.Excel文件：可以设置“比较项”，即Excel文件的工作簿（sheet）、列、行。每次添加一个“比较项”，将两两比较所选文件夹中的Excel文件" +
+                "\n  5.Excel文件：可以设置“比较项”，即Excel文件的工作簿（sheet）、列、行。每次添加一个“比较项”，将两两比较所选文件夹中的Excel文件" +
                 "中该“比较项”的文本。如果“比较项”中只有一个Excel文件中有，则不会进行比较。");
         txtpnrnrncsvexcelrn.setToolTipText("使用说明");
         txtpnrnrncsvexcelrn.setBounds(10, 55, 759, 300);
@@ -47,7 +60,7 @@ public class UiUtil {
         return txtpnrnrncsvexcelrn;
     }
 
-    public static Dimension computePreferredSize(JPanel panel){
+    public static Dimension computePreferredSize(JPanel panel) {
 
         // compute preferred size
         Dimension preferredSize = new Dimension();
